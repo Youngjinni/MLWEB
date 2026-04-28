ba41edb9681c7b68be375d68477f31b2109c6c85
PC <lkjmnh1283@gmail.com>
2026-04-28 10:36:16 +0900

    commit

--- backend/gateway-service/src/main/java/com/ml_app/gatewayservice/GatewayServiceApplication.java
+++ backend/gateway-service/src/main/java/com/ml_app/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,12 @@ package com.ml_app.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// 💡 핵심: DB 자동 설정을 아예 사용하지 않겠다고 선언합니다.
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GatewayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
 }
